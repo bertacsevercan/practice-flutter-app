b305d076c77b69e6776db3ac6ce3137f5ddfe2da
bertacsevercan <bertac.severcan@gmail.com>
2021-04-29 16:09:16 +0300

    rename myapp to home

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,44 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
 
-class MyApp extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.pink[200],
-        appBar: AppBar(
-          title: Text(
-            "Practice App",
-            style: TextStyle(
-              fontFamily: "EmilysCandy",
-              fontSize: 22.0,
+    return Scaffold(
+      backgroundColor: Colors.pink[200],
+      appBar: AppBar(
+        title: Text(
+          "Practice App",
+          style: TextStyle(
+            fontFamily: "EmilysCandy",
+            fontSize: 22.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+          ),
+        ),
+        backgroundColor: Colors.pink,
+      ),
+      body: Center(
+        child: Text(
+          "helloo",
+          style: TextStyle(
+              fontSize: 20.0,
               fontWeight: FontWeight.bold,
               letterSpacing: 2.0,
-            ),
-          ),
-          backgroundColor: Colors.pink,
+              color: Colors.white,
+              fontFamily: "EmilysCandy"),
         ),
-        body: Center(
-          child: Text(
-            "helloo",
-            style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.white,
-                fontFamily: "EmilysCandy"),
-          ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => null,
+        child: Text(
+          "+",
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => null,
-          child: Text(
-            "+",
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.pink,
-        ),
+        backgroundColor: Colors.pink,
       ),
     );
   }
