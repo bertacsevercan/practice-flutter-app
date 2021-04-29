d7c9197c6042a06416dbc20ebe421188b73c41b3
bertacsevercan <bertac.severcan@gmail.com>
2021-04-29 17:03:49 +0300

    create classes for each widget

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,8 +5,6 @@ void main() => runApp(MaterialApp(
     ));
 
 class Home extends StatelessWidget {
-  final imgSrc =
-      "https://images.unsplash.com/photo-1514517521153-1be72277b32f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,32 +21,73 @@ class Home extends StatelessWidget {
         ),
         backgroundColor: Colors.pink,
       ),
-      body: Center(
-          child: ElevatedButton.icon(
-        //TextButton, IconButton...
-        icon: Icon(Icons.drag_handle),
-        label: Text("Tap me"),
-        onPressed: () => print("Tapped"),
-      ) //Image.asset("images/lollipop.jpg"),
-          /* Text(
-          "helloo",
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.white,
-              fontFamily: "EmilysCandy"),
-        ), */
-          ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print("Honk!"),
-        child: Icon(
-          Icons.airport_shuttle,
-          color: Colors.lightBlue[200],
-          size: 30.0,
-        ),
-        backgroundColor: Colors.pink,
+      body: _Container(), //Center(child: _Text()),
+      floatingActionButton: _FloatingActionButton(),
+    );
+  }
+}
+
+class _Text extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "helloo",
+        style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.white,
+            fontFamily: "EmilysCandy"),
       ),
+    );
+  }
+}
+
+class _Container extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
+
+class _FloatingActionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () => print("Honk!"),
+      child: Icon(
+        Icons.airport_shuttle,
+        color: Colors.lightBlue[200],
+        size: 30.0,
+      ),
+      backgroundColor: Colors.pink,
+    );
+  }
+}
+
+class _AssetImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Image.asset("images/lollipop.jpg");
+}
+
+class _NetworkImage extends StatelessWidget {
+  final imgSrc =
+      "https://images.unsplash.com/photo-1514517521153-1be72277b32f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80";
+  @override
+  Widget build(BuildContext context) => Image.network(imgSrc);
+}
+
+class _ElevatedButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      //TextButton, IconButton...
+      icon: Icon(Icons.drag_handle),
+      label: Text("Tap me"),
+      onPressed: () => print("Tapped"),
     );
   }
 }
