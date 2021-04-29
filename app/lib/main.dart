dc2dbacdca09068c3cd60d079ffddb7e059cef41
bertacsevercan <bertac.severcan@gmail.com>
2021-04-29 15:33:28 +0300

    add a new font and add text style to app

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -7,15 +7,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.pink[300],
+        backgroundColor: Colors.pink[200],
         appBar: AppBar(
-          title: Text("Practice App"),
+          title: Text(
+            "Practice App",
+            style: TextStyle(
+              fontFamily: "EmilysCandy",
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+            ),
+          ),
+          backgroundColor: Colors.pink,
         ),
         body: Center(
-          child: Text("helloo"),
+          child: Text(
+            "helloo",
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.white,
+                fontFamily: "EmilysCandy"),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          child: Text("+"),
+          onPressed: () => null,
+          child: Text(
+            "+",
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.pink,
         ),
       ),
     );
