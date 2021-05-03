a36b21586db07ce8fbdf276d3f4320706ebd0806
bertacsevercan <bertac.severcan@gmail.com>
2021-05-03 16:10:39 +0300

    output data on UI

--- app/lib/WorldTimeApp/pages/home.dart
+++ app/lib/WorldTimeApp/pages/home.dart
@@ -13,13 +13,41 @@ class _HomeState extends State<Home> {
     print(data);
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            TextButton.icon(
-                onPressed: () => Navigator.pushNamed(context, "/location"),
-                label: Text("Choose location"),
-                icon: Icon(Icons.edit_location))
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(top: 120.0),
+          child: Column(
+            children: <Widget>[
+              TextButton.icon(
+                  onPressed: () => Navigator.pushNamed(context, "/location"),
+                  label: Text("Choose location"),
+                  icon: Icon(Icons.edit_location)),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    data["location"],
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      letterSpacing: 2.0,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                data["time"],
+                style: TextStyle(
+                  fontSize: 64.0,
+                  letterSpacing: 2.0,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- app/lib/WorldTimeApp/services/WorldTime.dart
+++ app/lib/WorldTimeApp/services/WorldTime.dart
@@ -1,5 +1,6 @@
 import "package:http/http.dart";
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String location;
@@ -21,7 +22,8 @@ class WorldTime {
       //print(datetime + offset);
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
-      time = now.toString();
+      //time = now.toString();
+      time = DateFormat.jm().format(now);
       print("Got the data.");
     } catch (e) {
       print("Error: $e");
